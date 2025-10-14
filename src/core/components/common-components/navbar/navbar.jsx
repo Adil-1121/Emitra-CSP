71d730551b8323e8d8e23c49a8f90fa4b0de6783
Adil Mohammad <aadilsilawat200021@gmail.com>
2025-10-14 19:57:53 +0530

    updated search box

--- src/core/components/common-components/navbar/navbar.jsx
+++ src/core/components/common-components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -17,10 +17,14 @@ import MessagesBox from "../../navbar-components/message-box/Messages";
 import LanguageDropdown from "../../navbar-components/language-box/LanguageDropdown";
 import AddNewDropdown from "../../navbar-components/add-dropdown-box/AddNewDropdown";
 import ProfileAvatar from "../../navbar-components/profile-avatar/ProfileAvatar";
+import { userRows } from "../../../../datatablesource";
 
 const Navbar = () => {
     const { dispatch, darkMode } = useContext(DarkModeContext);
     const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
 
     const toggleFullscreen = () => {
         if (!document.fullscreenElement) {
@@ -30,13 +34,57 @@ const Navbar = () => {
         }
     };
 
+    useEffect(() => {
+        if (searchTerm.length > 0) {
+            const filteredUsers = userRows.filter(user => 
+                user.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.email.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setSearchResults(filteredUsers.slice(0, 5));
+            setShowDropdown(true);
+        } else {
+            setSearchResults([]);
+            setShowDropdown(false);
+        }
+    }, [searchTerm]);
+
+    const handleUserClick = (userId) => {
+        navigate(`/users/view-user/${userId}`);
+        setSearchTerm("");
+        setShowDropdown(false);
+    };
+
     return (
         <div className="navbar">
             <div className="wrapper">
                 {/* Search */}
                 <div className="search">
-                    <input type="text" placeholder="Type here to search..." />
+                    <input 
+                        type="text" 
+                        placeholder="Search users..." 
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
+                    />
                     <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                    {showDropdown && searchResults.length > 0 && (
+                        <div className="search-dropdown">
+                            {searchResults.map(user => (
+                                <div 
+                                    key={user.id} 
+                                    className="search-item"
+                                    onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
+
+                                >
+                                    <img src={user.img} alt={user.userName} className="search-avatar" />
+                                    <div className="search-info">
+                                        <span className="search-name">{user.userName}</span>
+                                        <span className="search-email">{user.email}</span>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
 
                 <div className="items">
